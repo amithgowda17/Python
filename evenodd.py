7eb2a076a672b0c650846eed9e33bb4ffc310fa3
amithgowda17 <amith.s.xworkz@gmail.com>
2026-05-14 20:40:56 +0530

    if else else if and or not operator

--- evenodd.py
+++ evenodd.py
@@ -6,3 +6,15 @@ if number%2==0:
     print("even number")
 else:
     print("odd number")
+
+
+# multiple condition like and or and not
+# a = 5
+# b = 7
+#
+# if a >= b and a != b:
+#     print("A")
+# elif not a >= b and a != b:
+#     print("B")
+# else:
+#     print("C")
