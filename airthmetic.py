d02448c98075204782883c28fe5b45452b386f9e
amithgowda17 <amith.s.xworkz@gmail.com>
2026-09-09 22:34:21 +0530

    airthmetic operations

---
+++ airthmetic.py
@@ -0,0 +1,22 @@
+# Write a Python program that performs basic arithmetic operations
+# (addition, subtraction, multiplication, and division) on two numbers.
+# Define the two numbers as variables within the code and print the results
+# for each operation.
+
+firstBowl=10;
+secondBowl=20;
+
+print(firstBowl+secondBowl);
+print(firstBowl-secondBowl);
+print(firstBowl*secondBowl);
+print(firstBowl/secondBowl);
+
+# Write a Python program that swaps the values of two
+# variables with and without using a third variable.
+
+a=10
+b=3
+print("before swapping:",a,b)
+
+a,b=b,a
+print(a,b)
